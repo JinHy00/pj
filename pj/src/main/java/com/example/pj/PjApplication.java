a77bd94ffeb70bf1cacc59d08234bb02f9c99e55
dabin Lee <dabin4132@naver.com>
2025-02-25 16:01:32 +0900

    빈

--- pj/src/main/java/com/example/pj/PjApplication.java
+++ pj/src/main/java/com/example/pj/PjApplication.java
@@ -1,7 +1,15 @@
 package com.example.pj;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @SpringBootApplication
 public class PjApplication {
@@ -10,4 +18,17 @@ public class PjApplication {
 		SpringApplication.run(PjApplication.class, args);
 	}
 
+	@Bean
+	SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(dataSource);
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*.xml");
+		bean.setMapperLocations(res);
+		return bean.getObject();
+	}
+	
+	@Bean
+	SqlSessionTemplate sqlSession(SqlSessionFactory factory) {
+		return new SqlSessionTemplate(factory);
+	}
 }
