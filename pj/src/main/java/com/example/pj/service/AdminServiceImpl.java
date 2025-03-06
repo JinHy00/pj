fa606d40bab44310d1d1836f8ee1b22356de1ade
dabin Lee <dabin4132@naver.com>
2025-03-06 11:51:53 +0900

    admin

--- pj/src/main/java/com/example/pj/service/AdminServiceImpl.java
+++ pj/src/main/java/com/example/pj/service/AdminServiceImpl.java
@@ -5,20 +5,22 @@ package com.example.pj.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.pj.mapper.AdminMapper;
+
 @Service
 public class AdminServiceImpl implements AdminService {
 	@Autowired
-	AdminService adminService;
+	AdminMapper adminMapper;
 		
 	@Override
 	public String loginAdmin(String adminid, String passwd) {
-		String name = adminService.loginAdmin(adminid, passwd);
-			return name;
+		String name = adminMapper.loginAdmin(adminid, passwd);
+		return name;
 	}
 
 	@Override
 	public int passwdCheck(String adminid, String passwd) {
-		int result = adminService.passwdCheck(adminid, passwd);
+		int result = adminMapper.passwdCheck(adminid, passwd);
 		return result;
 	}
 
