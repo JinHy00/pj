aaabfe0fbc38f79abd498aabfaa15bd135c1b467
dabin Lee <dabin4132@naver.com>
2025-03-07 02:15:37 +0900

    product

--- pj/src/main/java/com/example/pj/service/ProductService.java
+++ pj/src/main/java/com/example/pj/service/ProductService.java
@@ -1,35 +1,34 @@
 package com.example.pj.service;
 
 import java.util.List;
-
 import com.example.pj.dto.ProductDTO;
 
 public interface ProductService {
-      // 상품리스트
-      List<ProductDTO> list(String searchkey, String keyword);
-      
-      // 브랜드 상품리스트
-      List<ProductDTO> brandList(int brandCode, String keyword);
-      
-      // 카테고리(성별) 상품리스트
-      List<ProductDTO> genderList(String gender, String searchkey, String keyword);
+    // 상품리스트
+    List<ProductDTO> list(String searchkey, String keyword);
+    
+    // 브랜드 상품리스트
+    List<ProductDTO> brandList(int brandCode, String keyword);
+    
+    // 카테고리(성별) 상품리스트
+    List<ProductDTO> genderList(String gender, String searchkey, String keyword);
 
-      // 상품정보
-      ProductDTO detail(int productCode);
-      
-      // 상품등록
-      void insert(ProductDTO dto);
-      
-      // 상품수정
-      void update(ProductDTO dto);
-      
-      // 상품삭제
-      void delete(int productCode);
-      
-      // 추가 안할 수도 
-      // 이미지1
-      String mainImg(int productCode);
-      
-      // 이미지2
-      String detailImg(int productCode); 
+    // 상품정보
+    ProductDTO detail(int productCode);
+    
+    // 상품등록
+    void insert(ProductDTO dto);
+    
+    // 상품수정
+    void update(ProductDTO dto);
+    
+    // 상품삭제
+    void delete(int productCode);
+    
+    // 추가 안할 수도 
+    // 이미지1
+    String mainImg(int productCode);
+    
+    // 이미지2
+    String detailImg(int productCode); 
 }
--- pj/src/main/java/com/example/pj/service/ProductServiceImpl.java
+++ pj/src/main/java/com/example/pj/service/ProductServiceImpl.java
@@ -12,64 +12,61 @@ import com.example.pj.mapper.ProductMapper;
 
 @Service
 public class ProductServiceImpl implements ProductService {
-   @Autowired
-   ProductMapper productMapper;
-   
-   @Override
-   public List<ProductDTO> list(String searchkey, String keyword) {
-      Map<String, Object> map = new HashMap<>();
-      map.put("searchkey", searchkey);
-      map.put("keyword", keyword);
-      return productMapper.list(map);
-   }
-   
-   @Override
-   public List<ProductDTO> brandList(int brandCode, String keyword) {
-      Map<String, Object> map = new HashMap<>();
-      map.put("keyword", keyword);
-      map.put("brandCode", brandCode);
-      return productMapper.brandList(map);
-   }
-   
-   @Override
-   public List<ProductDTO> genderList(String gender,  String searchkey, String keyword) {
-      Map<String, Object> map = new HashMap<>();
-      map.put("searchkey", searchkey);
-      map.put("keyword", keyword);
-      map.put("gender", gender);
-      return productMapper.genderList(map);
-   }
+    @Autowired
+    ProductMapper productMapper;
+    
+    @Override
+    public List<ProductDTO> list(String searchkey, String keyword) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("searchkey", searchkey);
+        map.put("keyword", keyword);
+        return productMapper.list(map);
+    }
+    
+    @Override
+    public List<ProductDTO> brandList(int brandCode, String keyword) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("keyword", keyword);
+        map.put("brandCode", brandCode);
+        return productMapper.brandList(map);
+    }
+    
+    @Override
+    public List<ProductDTO> genderList(String gender,  String searchkey, String keyword) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("searchkey", searchkey);
+        map.put("keyword", keyword);
+        map.put("gender", gender);
+        return productMapper.genderList(map);
+    }
 
-   @Override
-   public ProductDTO detail(int productCode) {
-      return productMapper.detail(productCode);
-   }
+    @Override
+    public ProductDTO detail(int productCode) {
+        return productMapper.detail(productCode);
+    }
 
-   @Override
-   public void insert(ProductDTO dto) {
-      productMapper.insert(dto);
-      
-      
-   }
+    @Override
+    public void insert(ProductDTO dto) {
+        productMapper.insert(dto);
+    }
 
-   @Override
-   public void update(ProductDTO dto) {
-      productMapper.update(dto);
+    @Override
+    public void update(ProductDTO dto) {
+        productMapper.update(dto);
+    }
 
-   }
+    @Override
+    public void delete(int productCode) {
+        productMapper.delete(productCode);
+    }
 
-   @Override
-   public void delete(int productCode) {
-      productMapper.delete(productCode);
-   }
+    @Override
+    public String mainImg(int productCode) {
+        return productMapper.mainImg(productCode);
+    }
 
-   @Override
-	public String mainImg(int productCode) {
-		return productMapper.mainImg(productCode);
-	}
-   
-   @Override
-	public String detailImg(int productCode) {
-		return productMapper.detailImg(productCode);
-	}
+    @Override
+    public String detailImg(int productCode) {
+        return productMapper.detailImg(productCode);
+    }
 }
