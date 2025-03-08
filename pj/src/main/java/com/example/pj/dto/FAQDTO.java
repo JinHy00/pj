1662d5606d5876c2128d1fb53519553f11e999f7
EunKyung123 <dbdmsrud02@naver.com>
2025-03-08 21:13:20 +0900

    a

--- pj/src/main/java/com/example/pj/dto/FAQDTO.java
+++ pj/src/main/java/com/example/pj/dto/FAQDTO.java
@@ -14,4 +14,40 @@ public class FAQDTO {
 	private String faqContent;
 	private int f_categoryCode;
 	private String f_categoryName;
+	public int getFaqCode() {
+		return faqCode;
+	}
+	public void setFaqCode(int faqCode) {
+		this.faqCode = faqCode;
+	}
+	public String getAdminid() {
+		return adminid;
+	}
+	public void setAdminid(String adminid) {
+		this.adminid = adminid;
+	}
+	public String getFaqTitle() {
+		return faqTitle;
+	}
+	public void setFaqTitle(String faqTitle) {
+		this.faqTitle = faqTitle;
+	}
+	public String getFaqContent() {
+		return faqContent;
+	}
+	public void setFaqContent(String faqContent) {
+		this.faqContent = faqContent;
+	}
+	public int getF_categoryCode() {
+		return f_categoryCode;
+	}
+	public void setF_categoryCode(int f_categoryCode) {
+		this.f_categoryCode = f_categoryCode;
+	}
+	public String getF_categoryName() {
+		return f_categoryName;
+	}
+	public void setF_categoryName(String f_categoryName) {
+		this.f_categoryName = f_categoryName;
+	}
 }
--- pj/src/main/java/com/example/pj/dto/QnADTO.java
+++ pj/src/main/java/com/example/pj/dto/QnADTO.java
@@ -20,4 +20,64 @@ public class QnADTO {
 	private String q_categoryName;
 	private MultipartFile img;
 	private QnAReplyDTO qnaReply;
+	public int getQnaCode() {
+		return qnaCode;
+	}
+	public void setQnaCode(int qnaCode) {
+		this.qnaCode = qnaCode;
+	}
+	public String getQnaTitle() {
+		return qnaTitle;
+	}
+	public void setQnaTitle(String qnaTitle) {
+		this.qnaTitle = qnaTitle;
+	}
+	public String getQnaContent() {
+		return qnaContent;
+	}
+	public void setQnaContent(String qnaContent) {
+		this.qnaContent = qnaContent;
+	}
+	public String getQnaDate() {
+		return qnaDate;
+	}
+	public void setQnaDate(String qnaDate) {
+		this.qnaDate = qnaDate;
+	}
+	public String getQnaFile() {
+		return qnaFile;
+	}
+	public void setQnaFile(String qnaFile) {
+		this.qnaFile = qnaFile;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public int getQ_categoryCode() {
+		return q_categoryCode;
+	}
+	public void setQ_categoryCode(int q_categoryCode) {
+		this.q_categoryCode = q_categoryCode;
+	}
+	public String getQ_categoryName() {
+		return q_categoryName;
+	}
+	public void setQ_categoryName(String q_categoryName) {
+		this.q_categoryName = q_categoryName;
+	}
+	public MultipartFile getImg() {
+		return img;
+	}
+	public void setImg(MultipartFile img) {
+		this.img = img;
+	}
+	public QnAReplyDTO getQnaReply() {
+		return qnaReply;
+	}
+	public void setQnaReply(QnAReplyDTO qnaReply) {
+		this.qnaReply = qnaReply;
+	}
 }
