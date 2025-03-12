4c39c75d5a260329bf6cde799620c5c6790649ff
EunKyung123 <dbdmsrud02@naver.com>
2025-03-12 22:37:17 +0900

    wish

--- pj/src/main/java/com/example/pj/service/WishService.java
+++ pj/src/main/java/com/example/pj/service/WishService.java
@@ -1,5 +1,17 @@
 package com.example.pj.service;
 
+import java.util.List;
+import java.util.Map;
+
+import com.example.pj.dto.WishDTO;
+
 public interface WishService {
 
+	List<WishDTO> list(String userid);
+	
+	int count(String userid, int productCode);
+	
+	// String insert(WishDTO dto);
+	
+	void delete(int wishCode, String userid);
 }
