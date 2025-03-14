73e517ec90c40a7d51a6e7ad9e15b515f9f9a45c
dabin Lee <dabin4132@naver.com>
2025-03-14 21:52:04 +0900

    cart

--- pj/src/main/java/com/example/pj/dto/CartDTO.java
+++ pj/src/main/java/com/example/pj/dto/CartDTO.java
@@ -11,10 +11,10 @@ public class CartDTO {
     private int cartCode;
     private String userId;
     private int productCode;
-    private int selectedCapacity;
     private int cartAmount;
-    private int finalPrice;  // 최종 가격은 DB에서 계산됨
+    private int capacity;
+    private int finalPrice;  
     private String productName;
     private String mainImage;
-    private int price;  // 기본 가격
+    private int price;  
 }
